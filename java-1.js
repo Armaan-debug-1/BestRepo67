0c3766c4231f915993711952c2282359f70ccafa
Armaan-debug-1 <guptaarmaan1505@gmail.com>
2025-11-12 23:26:33 +0530

    program for post incrementation and pre increment.

---
+++ java-1.js
@@ -0,0 +1,7 @@
+const accId = 1234
+let accEmail="Name@example.com"
+var accPassword="Hello123"
+accountCity = "Mussoorie"
+console.table([accEmail,accPassword,accountCity])
+// do not use var as it has problems with scope
+
---
+++ java-2.js
@@ -0,0 +1,13 @@
+"use strict" //treat all JS code as newer version
+const hi = 12
+console.log(hi);
+let num1= 12
+let num2 = 10
+// console.log(num1,num2);
+num1=num2++
+num2 = ++num1
+console.table([num1,num2])
+
+
+
+ 
