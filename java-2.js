8a076b72eec9bfeac6ffc0ca8e34bb047ba31d71
Armaan-debug-1 <guptaarmaan1505@gmail.com>
2025-11-14 22:40:17 +0530

    logged basic boolean results to the console

--- java-2.js
+++ java-2.js
@@ -7,7 +7,18 @@ let num2 = 10
 num1=num2++
 num2 = ++num1
 console.table([num1,num2])
+/*
+Primitive Data Types
+7 types: string,null,Boolean,undefined,number,symbol,BigInt
 
-
+Reference: Non-primitive
+Array
+Objects
+Functions
+*/
+const id = Symbol('59')
+const id2 = Symbol('59')
+let count=10
+let score=20
 
  
