fc1332bc093e5c16a7e5d7e5d3bdf7a44d436ab0
Armaan-debug-1 <guptaarmaan1505@gmail.com>
2026-01-02 18:09:57 +0530

    add filtering, mapping, and reduction examples to JavaScript loops

--- java-loops.js
+++ java-loops.js
@@ -70,7 +70,7 @@ for (const key in User) {
 //IMPORTANT - foreach loop
 const coding = ["js","ruby","swift"]
 coding.forEach((item) => {
-    console.log(item);
+    //console.log(item);
 })
 
 // if it was an array of objects
@@ -79,3 +79,57 @@ coding.forEach((item) => {
 })
 
 // NOTE: Other functions defined outside the ForEach loop can also be used. But they have to be referenced.
+// filtering arrays
+const nums = [1,2,3,9,10]
+const smallnums = nums.filter( (num) => num < 5)
+// console.log(smallnums);
+// if scope is opened ({}) , then return keyword is necessary.
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+
+  const scbooks = books.filter( (b) => b.publish < 2000)
+  scbooks.forEach((b) => {
+    //console.log(b.title);
+    
+  })
+
+  //map property of arrays
+  let nums1 = [1,2,3,4,5]
+  nums1 = nums1.map((i) => i + 10)  
+  // chaining = applying multiple properties (same or different) to an array
+  nums1=nums1.map((num) => num+10).filter((num) => num >22)
+    // console.log(nums1);
+
+//reduction
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12990
+    },
+]
+
+const initial = 0
+const total = shoppingCart.reduce((acc,item) =>  acc + item.price,0)
+console.log(`hi, your total today is ${total} unfortunately :>`);
